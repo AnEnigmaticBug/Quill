758d2d589eefb5f7dfe54c879ab58b05e444ad24
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-12-31 18:18:37 +0530

    Set MainActivity to display NoteListFragment

--- app/src/main/java/com/anenigmaticbug/quill/MainActivity.kt
+++ app/src/main/java/com/anenigmaticbug/quill/MainActivity.kt
@@ -2,11 +2,18 @@ package com.anenigmaticbug.quill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anenigmaticbug.quill.screens.notelist.view.NoteListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_main)
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.navHostPOV, NoteListFragment())
+                .commit()
+        }
     }
 }

3c4296be62e21e055e71dade36511d70e05e02c4
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-12-31 10:58:52 +0530

    Make Dagger2 provide the Room database + NoteDao

--- app/src/main/java/com/anenigmaticbug/quill/di/shared/AppModule.kt
+++ app/src/main/java/com/anenigmaticbug/quill/di/shared/AppModule.kt
@@ -1,12 +1,26 @@
 package com.anenigmaticbug.quill.di.shared
 
 import android.app.Application
+import androidx.room.Room
+import com.anenigmaticbug.quill.screens.shared.data.room.AppDatabase
+import com.anenigmaticbug.quill.screens.shared.data.room.NoteDao
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
 class AppModule(private val application: Application) {
+
+    @Provides @Singleton
+    fun providesNoteDao(appDatabase: AppDatabase): NoteDao {
+        return appDatabase.noteDao()
+    }
+
+    @Provides @Singleton
+    fun providesAppDatabase(application: Application): AppDatabase {
+        return Room.databaseBuilder(application, AppDatabase::class.java, "quill.db")
+            .build()
+    }
 
     @Provides @Singleton
     fun providesApplication(): Application {
